f695e8f2b9b9ccaaf5c9bc6b4bfb510c25441d32
HengHo <hengza55514@gmail.com>
2023-04-24 18:07:18 +0700

    24

--- src/pages/HomeTEST.vue
+++ src/pages/HomeTEST.vue
@@ -1,7 +1,7 @@
 <template>
   <q-page>
     <div class="box-bg" id="Home">
-      <img class="dimmed fit q-mt-lg" src="../assets/ปก.jpg" />
+      <img class="dimmed fit q-mt-lg" src="../assets/รวมปก.png" />
     </div>
 
     <div class="q-gutter-xs row justify-center">
@@ -12,6 +12,7 @@
         @click="toggleTrue"
         label="new"
       />
+      <q-select outlined v-model="Year" :options="options" color=secondary />
       <q-btn
         flat
         :class="sel ? 'hide' : 'show'"
@@ -21,35 +22,49 @@
       />
     </div>
 
-    <section v-for="(item, index) in yearsList" :key="index">
+    <section v-for="(item, index) in majorList" :key="index">
       <p class="text-h6">
-        {{ item.year_yearbook }}
+        {{ item.name }}
       </p>
-      <section v-for="(item, index) in majorList" :key="index" class="q-pl-xl">
-        <p>
-          {{ item.name }}
-        </p>
-        <div class="q-gutter-xl row q-mb-xl">
-          <q-card v-for="(subitems, subindex) in item.classle" :key="subindex"  class="my-card col-3">
-            <q-img src="https://cdn.quasar.dev/img/parallax2.jpg">
+      <section class="q-pl-xl">
+        <div v-for="(subitems, subindex) in item.classle" :key="subindex" class="q-gutter-xl row q-mb-xl">
+          <q-card v-if="subitems.classle != null" class="my-card col-3">
+            <q-img :src=subitems.picture.path >
               
             </q-img>
-            <p class="row justify-center">{{ subitems.class}}{{ subitems.major }}</p>
+            <q-img src="../assets/images.png"></q-img>
+            
+            <p class="row justify-center">
+          {{ subitems.class}} 
+        </p>
           </q-card>
           
         </div>
       </section>
-      <p></p>
+     
+      
+      
+      
     </section>
     <!-- <div class="grainer-to-page"></div> -->
     <!-- ------------------------------------------yearbook------------------------------------------------- -->
+    
     <q-page-scroller
       position="bottom-right"
       :scroll-offset="150"
       :offset="[18, 18]"
+      
     >
-      <q-btn fab icon="keyboard_arrow_up" color="accent" />
+    
+    <img
+    srcset="../assets/เด็กถือป้าย-removebg-preview.png 2000w">
+   
+  
+      <!-- <q-btn fab icon="keyboard_arrow_up" color="accent" /> -->
     </q-page-scroller>
+
+
+
   </q-page>
 </template>
 
@@ -61,26 +76,27 @@ import {
   biCaretDownFill,
   biPerson,
 } from "@quasar/extras/bootstrap-icons";
-import { ref, onMounted } from "vue";
+import { ref, onMounted, watch, computed} from "vue";
 import { useRouter } from "vue-router";
 import { scroll } from "quasar";
 import { YearbookApi } from "src/api/YearbookApi";
 import { list } from "postcss";
+import { LocalStorage } from "quasar";
 
-const Year = ref([]);
+const Year = ref([LocalStorage.getItem("year")]);
 const slide = ref(1);
 const { MajorList, YearList } = YearbookApi();
-const loading = ref(false);
+const loading = ref(false); 
 const yearsList = ref([]);
 const majorList = ref([]);
 const routet = useRouter();
 const sel = ref(true);
 const List = ref([]);
-
+const options = ref([]);
 onMounted(async () => {
   fetchYears();
   fetchMajor();
-  
+
 });
 
 const toggleTrue = () => {
@@ -106,43 +122,33 @@ const fetchYears = async () => {
   const respone = await YearList();
   loading.value = false;
   if (respone) {
-    yearsList.value = respone.dataList;
-    // Year.value = yearsList.value.year_yearbook
-    console.log("year",yearsList.value);
-    for(let y of yearsList.value){
-      console.log('aaaa',y.year_yearbook)
-      let numb = Number(y.year_yearbook)
-      const test = await MajorList( numb) 
-      if(test){
-        console.log("test",test)
-        List.value = test.dataList
-        console.log("List",List.value._rawValue);
-      }
-    }
+    yearsList.value = respone.dataList
+    yearsList.value.forEach(item => {
+      options.value.push(item.year_yearbook)
+    })
   }
 
-  // console.log("YearList", respone);
+  // console.log("YearList", respone.dataList)
+  // console.log("yearlist",yearsList.value)
+  // console.log(options)
 };
 
 const fetchMajor = async () => {
   loading.value = true;
-  Year.value.forEach(item => {
-    console.log("ttt",item)
-  })
+  const respone = await MajorList(Year.value);
   loading.value = false;
-  // if (respone) {
-  //   majorList.value = respone.dataList;
-  // }
+  if (respone) {
+    majorList.value = respone.dataList;
+  }
 
-  // console.log("majorList", respone);
-  // console.log("year",Year.value)
+  console.log("majorList", respone)
 };
-// watch(Year, async (newVal, oldVal) => {
-//   console.log("count changed from ", oldVal, newVal);
-//   if (newVal > 10) {
-//     console.log('value greater than 10');
-//   }
-// });
+watch(Year, async (newVal, oldVal) => {
+  console.log("count changed from ", oldVal, newVal);
+  LocalStorage.set("year",newVal);
+  fetchMajor();
+});
+
 </script>
 
 <style scoped>
